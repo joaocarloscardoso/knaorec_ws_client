93a398431b4875a4d63c876a9af703564f6c7e55
Joao Cardoso <joao.ccardoso@gmail.com>
2021-02-19 23:59:52 +0000

    Client develop 20210219

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+//var app = require('express')();
+//var http = require('http').Server(app);
+var service = require('./lib/service.js');
+
+var app = express();
+
+// body parser middleware
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+app.listen(3003,function(){
+    console.log('Server started on port 3003...');
+    //service.GetPortfolios();
+    //service.AddPortfolio('Portfolio03');
+    //service.DeletePortfolio('Portfolio03');
+})
